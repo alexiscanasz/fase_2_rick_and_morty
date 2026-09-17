9f77069e84cd643c37abd79a366a12f3b9c5466d
Alexis Cañas <48936188+alexiscanasz@users.noreply.github.com>
2026-09-17 15:42:28 -0500

    refactor: agregar manejo de errores tipado con result/failure/success

--- lib/core/constants/api_constants.dart
+++ lib/core/constants/api_constants.dart
@@ -1,5 +1,5 @@
-class ApiConstants {
-  ApiConstants._();
+class RmApiConstants {
+  const RmApiConstants._();
 
   static const String baseUrl = 'https://rickandmortyapi.com/api';
   static const String charactersEndpoint = '$baseUrl/character';
---
+++ lib/core/core.dart
@@ -0,0 +1,2 @@
+export 'constants/api_constants.dart';
+export 'error/result.dart';
